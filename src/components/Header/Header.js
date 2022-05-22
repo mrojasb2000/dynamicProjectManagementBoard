2854bd14695aa7c338fdd9048727405a29438193
Mauricio Rojas <mrojase@vitamina.com>
2022-05-22 18:41:17 -0400

    feat: add task compoment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 function Header() {
   return (
     <div className="Header-wrapper">
-      <h1>Project Management Board</h1>
+      <h1>Dynamic Project Management Board</h1>
     </div>
   )
 }
--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'; 
 import Lane from '../../components/Lane/Lane';
 import './Board.css';
 
@@ -9,10 +10,38 @@ const lanes = [
 ];
 
 function Board() {
+  const [loading, setLoading] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+      async function fetchData() {
+        try {
+          const tasks = await fetch(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`);
+    
+          const result = await tasks.json();
+          console.log('result: ', result);
+          if (result) {
+            setTasks(result);
+            setLoading(false);
+          }
+        } catch (e) {
+          setLoading(false);
+          setError(e.message);
+        }
+      }
+      fetchData();
+  }, []);
+
   return (
     <div className='Board-wrapper'>
       {lanes.map((lane) => (
-        <Lane key={lane.id} title={lane.title} />
+        <Lane key={lane.id} 
+          title={lane.title} 
+          loading={loading} 
+          error={error} 
+          tasks={tasks.filter((task) => 
+            task.lane === lane.id)}/>
       ))}
     </div>
   );
